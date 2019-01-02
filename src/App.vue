9ba7b974dd2ff42bd0f40a438b61feaa1a714050
Dawid <dawid@wijata_laptop.localdomain>
2019-01-02 22:07:46 +0100

    Basics of basics of interface

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <div class="test" :style="blockColor">
+    
+    </div>
+    <div>
+      <input type="range" min="0" max="255" value="0" v-model="red">
+        <label for="">
+          RED
+        </label>
+    </div>
+    <div>
+      <input type="range" min="0" max="255" value="0" v-model="green"> 
+        <label for="">
+          GREEN
+        </label>
+    </div>
+    <div>
+      <input type="range" min="0" max="255" value="0" v-model="blue">
+        <label for="">
+          BLUE
+        </label>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      red: null,
+      green: null,
+      blue: null,
+      blockColor:{
+        backgroundColor: rgbGen(red, green, blue)
+      }
+    }
+  },
+  methods:{
+    rgbGen (red, green, blue){
+      return `rgb(${red}, ${green}, ${blue})`;
+    }
   }
 }
 </script>
 
 <style scoped>
-
+  .test{
+      width:100px;
+      height: 100px;
+  }
 </style>

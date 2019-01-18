0de8b7457b6d5d1cfca959cbf22bcd1f6fa4e845
Dawid <dawid@wijata_laptop.localdomain>
2019-01-18 21:57:58 +0100

    Workable app, just need to style css

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,37 @@
 <template>
   <div id="app">
+    <div class="container">
+      <div class="test" :style="{backgroundColor: rgbGen(red, green, blue) || hex_color}">
     
-    <div class="test" :style="{backgroundColor: rgbGen(red, green, blue)}">
-    
-    </div>
-    <div class="color_input">
-      <input type="range" min="0" max="255" v-model="red">
-      <label for="red_text">
-        RED
-      </label>
-      <input type="number" id="red_text" min="0" max="255" required v-model="red">
-    </div>
-    <div class="color_input">
-      <input type="range" min="0" max="255" v-model="green"> 
-      <label for="green_text">
-        GREEN
-      </label>
-      <input type="number" id="green_text" min="0" max="255" required v-model="green">
-    </div>
-    <div class="color_input">
-      <input type="range" min="0" max="255" v-model="blue">
-      <label for="blue_text">
-        BLUE
-      </label>
-      <input type="number" id="blue_text" min="0" max="255" required v-model="blue">
-    </div>
-    <div class="copy_output">
-      <label for="">Copy the color: </label>
-      <input type="text" v-model="hex_color">
+      </div>
+
+      <div class="color_input">
+        <input type="range" min="0" max="255" v-model="red">
+        <label for="red_text">
+          RED
+        </label>
+        <input type="number" id="red_text" min="0" max="255" required v-model="red">
+      </div>
+      <div class="color_input">
+        <input type="range" min="0" max="255" v-model="green"> 
+        <label for="green_text">
+          GREEN
+        </label>
+        <input type="number" id="green_text" min="0" max="255" required v-model="green">
+      </div>
+      <div class="color_input">
+        <input type="range" min="0" max="255" v-model="blue">
+        <label for="blue_text">
+          BLUE
+        </label>
+        <input type="number" id="blue_text" min="0" max="255" required v-model="blue">
+      </div>
+      <div class="copy_output">
+        <label for="">Your color: </label>
+        <input type="text" v-model="hex_color" class="copy_text" >
+        {{rgbToHex(red, green, blue)}}
+        <button @click="copyText()" >COPY</button>
+      </div>
     </div>
   </div>
 </template>
@@ -41,13 +45,30 @@ export default {
       red: Math.floor(Math.random()*255),
       green: Math.floor(Math.random()*255),
       blue: Math.floor(Math.random()*255),
-      hex_color: ''
+      hex_color: ``
     }
   },
   methods:{
     rgbGen: function(red, green, blue){
       return `rgb(${red}, ${green}, ${blue})`;
+    },
+
+    rgbToHex: function(red, green, blue){
+      let colorArr = [red || 0, green || 0, blue || 0];
+      
+      let colorString = colorArr.map(n => n < 256 ? parseInt(n).toString(16) : '255')
+                                .map(n => n.length < 2 ? '0' + n : n)
+                                .join('')
+
+      this.$data.hex_color = `#${colorString}`;
+    },
+
+    copyText: function(){
+      let copiedText = document.querySelector('.copy_text');
+      copiedText.select();
+      document.execCommand('copy');
     }
+
   }
 }
 </script>
@@ -59,16 +80,23 @@ export default {
     box-sizing: border-box;
   }
 
-  body{
-    height: 95vh;
-  }
-
   #app{
-
+    height: 100vh;
+    background-color: #666;
+    font-family: 'Lucida Sans', sans-serif;
+  }
+  
+  .container{
+    border: 1px black solid;
+    max-width: 295px;
+    padding: 30px 0;
+    margin: 0 auto;
+    transform: translateY(15vh);
+    background-color: white;
   }
 
   .test{
-      width: 100px;
+      width: 200px;
       height: 100px;
       margin: 0 auto 20px auto;
   }
@@ -93,6 +121,7 @@ export default {
 
   .color_input input[type=number]{
     width:55px;
+
   }
 
   .copy_output{
@@ -106,6 +135,7 @@ export default {
   }
 
   .copy_output input {
-    width: 60px;
+    width: 70px;
+    text-align: center;
   }
 </style>

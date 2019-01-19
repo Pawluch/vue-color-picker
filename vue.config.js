28f824e75d9fd4b3cfce2a23ca89b4108919a342
Dawid <dawid@wijata_laptop.localdomain>
2019-01-19 11:04:07 +0100

    Config changes

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  baseUrl: '/vue-color-picker'
+  baseUrl: '/vue-color-picker',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
